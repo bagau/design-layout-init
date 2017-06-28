c7e7492571531fcf742015bbcc74ab92c3e9bd60
Rafis Bagautdinov <bagau@users.noreply.github.com>
2017-06-28 10:31:04 +0500

    Upload gulpfile.js and package.json files

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+'use strict';
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+ 
+gulp.task('sass', function () {
+  gulp.src('./sass/inlnCustom.scss')
+  	.pipe(sourcemaps.init())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('./css'));
+});
+ 
+gulp.task('sass:watch', function () {
+  gulp.watch('./sass/**/*.scss', ['sass']);
+  gulp.watch('./sass/*.scss', ['sass']);
+  gulp.watch('./sass/**/*.sass', ['sass']);
+  gulp.watch('./sass/*.sass', ['sass']);
+});
