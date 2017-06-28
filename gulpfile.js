65256cb082954f49874c1a1ce98f8870da760e76
Rafis Bagautdinov <bagau@users.noreply.github.com>
2017-06-28 10:39:49 +0500

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -5,8 +5,8 @@ var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
  
 gulp.task('sass', function () {
-  gulp.src('./sass/inlnCustom.scss')
-  	.pipe(sourcemaps.init())
+  gulp.src('./sass/main.scss')
+    .pipe(sourcemaps.init())
     .pipe(sass().on('error', sass.logError))
     .pipe(sourcemaps.write())
     .pipe(gulp.dest('./css'));
